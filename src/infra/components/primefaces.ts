ce405fc6af30fb513e1e23661cfd728680b8456a
Tom G! <TheAutomaTom@gmail.com>
2022-08-02 10:58:27 -0500

    Clean upi PrimeFaces imports

--- src/infra/components/primefaces.ts
+++ src/infra/components/primefaces.ts
@@ -1,46 +1,48 @@
-import Button from "primevue/button";
-import Calendar from "primevue/calendar";
-import Card from "primevue/card";
-import CheckBox from "primevue/checkbox";
-import Column from "primevue/column";
-import DataTable from "primevue/datatable";
-import Dialog from "primevue/dialog";
+// import Button from "primevue/button";
+// import Calendar from "primevue/calendar";
+// import Card from "primevue/card";
+// import CheckBox from "primevue/checkbox";
+// import Column from "primevue/column";
+// import DataTable from "primevue/datatable";
+// import Dialog from "primevue/dialog";
 import Divider from "primevue/divider";
-import Dropdown from "primevue/dropdown";
-import FileUpload from "primevue/fileupload";
-import Galleria from "primevue/galleria";
-import InputMask from "primevue/inputmask";
-import InputNumber from "primevue/inputnumber";
-import InputSwitch from "primevue/inputswitch";
-import InputText from "primevue/inputtext";
-import MultiSelect from "primevue/multiselect";
-import Panel from "primevue/panel";
+// import Dropdown from "primevue/dropdown";
+// import FileUpload from "primevue/fileupload";
+// import Galleria from "primevue/galleria";
+// import InputMask from "primevue/inputmask";
+// import InputNumber from "primevue/inputnumber";
+// import InputSwitch from "primevue/inputswitch";
+// import InputText from "primevue/inputtext";
+// import MultiSelect from "primevue/multiselect";
+// import Panel from "primevue/panel";
+// import ProgressBar from "primevue/progressbar";
+// import ProgressSpinner from "primevue/progressspinner";
+// import RadioButton from "primevue/radiobutton";
+// import ScrollPanel from "primevue/scrollpanel";
 import ProgressBar from "primevue/progressbar";
-import ProgressSpinner from "primevue/progressspinner";
-import RadioButton from "primevue/radiobutton";
-import ScrollPanel from "primevue/scrollpanel";
 
 // Ultimately to be registered in main.ts for global access.
 export default [
-  { component: Button, name: "p-button" },
-  { component: Calendar, name: "p-calendar" },
-  { component: Card, name: "p-card" },
-  { component: CheckBox, name: "p-check-box" },
-  { component: Column, name: "p-column" },
-  { component: DataTable, name: "p-data-table" },
-  { component: Dialog, name: "p-dialog" },
-  { component: Divider, name: "p-hr" },
-  { component: Dropdown, name: "p-dropdown" },
-  { component: FileUpload, name: "p-file-upload" },
-  { component: Galleria, name: "p-galleria" },
-  { component: InputMask, name: "p-input-mask" },
-  { component: InputNumber, name: "p-input-number" },
-  { component: InputSwitch, name: "p-input-switch" },
-  { component: InputText, name: "p-input-text" },
-  { component: MultiSelect, name: "p-multi-select" },
-  { component: Panel, name: "p-panel" },
   { component: ProgressBar, name: "p-progress-bar" },
-  { component: ProgressSpinner, name: "p-progress-spinner" },
-  { component: RadioButton, name: "p-radio-button" },
-  { component: ScrollPanel, name: "p-scroll-panel" },
+  // { component: Button, name: "p-button" },
+  // { component: Calendar, name: "p-calendar" },
+  // { component: Card, name: "p-card" },
+  // { component: CheckBox, name: "p-check-box" },
+  // { component: Column, name: "p-column" },
+  // { component: DataTable, name: "p-data-table" },
+  // { component: Dialog, name: "p-dialog" },
+  { component: Divider, name: "p-hr" },
+  // { component: Dropdown, name: "p-dropdown" },
+  // { component: FileUpload, name: "p-file-upload" },
+  // { component: Galleria, name: "p-galleria" },
+  // { component: InputMask, name: "p-input-mask" },
+  // { component: InputNumber, name: "p-input-number" },
+  // { component: InputSwitch, name: "p-input-switch" },
+  // { component: InputText, name: "p-input-text" },
+  // { component: MultiSelect, name: "p-multi-select" },
+  // { component: Panel, name: "p-panel" },
+  // { component: ProgressBar, name: "p-progress-bar" },
+  // { component: ProgressSpinner, name: "p-progress-spinner" },
+  // { component: RadioButton, name: "p-radio-button" },
+  // { component: ScrollPanel, name: "p-scroll-panel" },
 ];
